cf7bf1ce2598b13048031fa7356d241e07f043cf
XinmingYe <369812012@qq.com>
2018-03-28 20:51:19 +0800

    first commit

---
+++ scriptsLib/u3dDemo1.air/u3dDemo1.py
@@ -0,0 +1,11 @@
+# -*- encoding=utf8 -*-
+__author__ = "User"
+__title__ = "test script title"
+__desc__ = """
+#this is a test script.
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco(force_restart=False)
+
+
+# start your script here
---
+++ scriptsLib/阿牛哥1.air/阿牛哥1.py
@@ -0,0 +1,69 @@
+# -*- encoding=utf-8 -*-
+__author__ = "User"
+__title__ = "test script title"
+__desc__ = """
+this is a test script.
+"""
+
+# start your script here
+#撒大大大苏打委屈饿
+touch(Template(r"tpl1521797130157.png", record_pos=(-0.338, -0.407), resolution=(1080.0, 1920.0)))
+sleep(1.0)
+
+touch(Template(r"tpl1521797153321.png", record_pos=(-0.007, 0.082), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797159572.png", record_pos=(-0.35, -0.055), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797161780.png", record_pos=(-0.359, -0.133), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797164371.png", record_pos=(-0.354, 0.027), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797166220.png", record_pos=(0.433, -0.223), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797169084.png", record_pos=(0.074, 0.017), resolution=(1920.0, 1080.0)))
+
+sleep(3.0)
+
+
+
+
+touch(Template(r"tpl1521797178361.png", record_pos=(-0.171, -0.182), resolution=(1920.0, 1080.0)))
+
+touch(Template(r"tpl1521807317278.png", record_pos=(-0.225, 0.244), resolution=(1920, 1080)))
+
+touch(Template(r"tpl1521807234907.png", record_pos=(-0.231, 0.053), resolution=(1920, 1080)))
+
+touch(Template(r"tpl1521805513306.png", record_pos=(0.002, 0.054), resolution=(1920, 1080)))
+touch(Template(r"tpl1521805520125.png", record_pos=(0.001, 0.116), resolution=(1920, 1080)))
+touch(Template(r"tpl1521805523584.png", record_pos=(0.002, 0.184), resolution=(1920, 1080)))
+touch(Template(r"tpl1521805528185.png", record_pos=(-0.001, 0.246), resolution=(1920, 1080)))
+touch(Template(r"tpl1521807104752.png", record_pos=(0.392, -0.164), resolution=(1920, 1080)))
+
+
+
+
+
+touch(Template(r"tpl1521805552788.png", record_pos=(0.005, 0.13), resolution=(1920, 1080)))
+
+touch(Template(r"tpl1521797511420.png", record_pos=(0.156, 0.131), resolution=(1920, 1080)))
+
+sleep(1.0)
+sleep(1.0)
+
+touch(Template(r"tpl1521797530039.png", record_pos=(-0.002, 0.071), resolution=(1920, 1080)))
+
+touch(Template(r"tpl1521797200472.png", record_pos=(0.284, -0.173), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797204317.png", record_pos=(0.074, -0.071), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797208787.png", record_pos=(0.007, 0.04), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797211032.png", record_pos=(0.157, 0.045), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797212867.png", record_pos=(0.254, 0.04), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797217843.png", record_pos=(-0.086, 0.04), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797220953.png", record_pos=(0.208, 0.242), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797225219.png", record_pos=(0.011, 0.084), resolution=(1920.0, 1080.0)))
+sleep(1.0)
+
+touch(Template(r"tpl1521797228255.png", record_pos=(0.472, 0.158), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797230264.png", record_pos=(0.361, 0.107), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797232681.png", record_pos=(0.467, -0.237), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797235304.png", record_pos=(-0.093, 0.184), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797237884.png", record_pos=(0.011, 0.061), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797241815.png", record_pos=(0.407, 0.246), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797244227.png", record_pos=(0.377, 0.186), resolution=(1920.0, 1080.0)))
+touch(Template(r"tpl1521797246768.png", record_pos=(0.145, 0.147), resolution=(1920.0, 1080.0)))
+keyevent("HOME")
+
